e3b4fa0c1d64e1aa5ff9f96d393137c6a323d7f9
IanLaptop <theholyboots@gmail.com>
2018-11-15 23:36:59 -0800

    Finished commit

--- DataScience/DataScience.py
+++ DataScience/DataScience.py
@@ -246,8 +246,11 @@ print(ts.plot())
 df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,
 columns=['A', 'B', 'C', 'D'])
 df = df.cumsum()
-print(plt.figure(); df.plot(); plt.legend(loc='best'))
+print(plt.figure())
 
+print(df.plot())
+
+print(plt.legend(loc='best'))
 
 
 
